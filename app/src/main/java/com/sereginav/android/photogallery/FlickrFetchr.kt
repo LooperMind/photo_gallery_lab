0eb8ca59693e95602e64c6c88cab730964d9eb88
LooperMind <sear_ring@mail.ru>
2022-12-14 13:20:49 +0700

    added get url funs

--- app/src/main/java/com/sereginav/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/sereginav/android/photogallery/FlickrFetchr.kt
@@ -1,11 +1,15 @@
 package com.sereginav.android.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.sereginav.android.photogallery.api.FlickrApi
 import com.sereginav.android.photogallery.api.FlickrResponse
 import com.sereginav.android.photogallery.api.PhotoResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -47,5 +51,15 @@ class FlickrFetchr {
             }
         })
         return responseLiveData
+    }
+
+    @WorkerThread
+    fun fetchPhotos(url: String): Bitmap?{
+        val response: Response<ResponseBody> =
+            flickrApi.fetchUrlBytes(url).execute()
+        val bitmap =
+            response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decode bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 }
